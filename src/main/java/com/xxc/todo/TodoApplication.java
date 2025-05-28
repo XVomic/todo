e296f034d4d8f5e66873106cf9abff19bc8878dd
xuxincheng <2961166903@qq.com>
2025-05-28 11:40:46 +0800

    增加mybatis扫描路径。解决无法获取bean的问题

--- src/main/java/com/xxc/todo/TodoApplication.java
+++ src/main/java/com/xxc/todo/TodoApplication.java
@@ -1,11 +1,13 @@
 package com.xxc.todo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.xxc.todo.dao")
+//也可以在配置文件中定义扫描路径
 public class TodoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
